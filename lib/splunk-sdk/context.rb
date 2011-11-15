2208ae49dbd2f5af87e18e2be43cb32d43588543
rdas <rob@splunk.com>
2011-11-15 11:22:55 -0800

    Refactored methods to use SSL certs if they are available

--- lib/splunk-sdk/context.rb
+++ lib/splunk-sdk/context.rb
@@ -1,5 +1,7 @@
 require 'rest-client'
 require 'libxml'
+require 'openssl'
+require './aloader'
 
 class Context
   DEFAULT_HOST = "localhost"
@@ -17,6 +19,8 @@ class Context
     @username = init_default(:username, '')
     @password = init_default(:password, '')
     @namespace = init_default(:namespace, nil)
+    @key_file = init_default(:key_file, nil)
+    @cert_file = init_default(:cert_file, nil)
     @headers = nil
   end
 
@@ -24,32 +28,35 @@ class Context
     response = post("/services/auth/login", {:username=>@username, :password=>@password})
     doc = LibXML::XML::Parser.string(response.to_s).parse
     @token = doc.find('//sessionKey').last.content
-    @headers = {'Authorization' => "Splunk #{@token}"}
+    @headers = {'Authorization' => "Splunk #{@token}", 'User-Agent' => 'splunk-sdk-ruby/0.1'}
   end
 
   def logout
-
+    @token = nil
   end
 
   def post(path, body, params={})
-    params.merge!(@headers) if !@headers.nil?
-    RestClient.post(url(path), body, params) do |response, request, result, &block|
+    resource = create_resource(path, params)
+    resource.post body, params do |response, request, result, &block|
       #TODO: Need error handling
       response
     end
   end
 
   def get(path, params={})
-    params.merge!(@headers) if !@headers.nil?
-    puts url(path)
-    RestClient.get(url(path), params) do |response, request, result, &block|
+    resource = create_resource(path, params)
+    resource.get params do |response, request, result, &block|
       #TODO: Need error handling
       response
     end
   end
 
   def delete(path, params)
-
+    resource = create_resource(path, params)
+    resource.delete params do |response, request, result, &block|
+      #TODO: Need error handling
+      response
+    end
   end
 
 
@@ -74,12 +81,29 @@ private
     "#{@prefix}#{fullpath(path)}"
   end
 
+  def create_resource(path, params)
+    params.merge!(@headers) if !@headers.nil?
+    if @key_file.nil? or @cert_file.nil?
+      resource = RestClient::Resource.new url(path)
+    else
+      resource = RestClient::Resource.new(
+          url(path),
+          :ssl_client_cert => OpenSSL::X509::Certificate.new(File.read(@cert_file)),
+          :ssl_client_key => OpenSSL::PKey::RSA.new(File.read(@key_file))
+          #:verify_ssl => OpenSSL::SSL::VERIFY_PEER
+      )
+    end
+    resource
+  end
 
 end
 
 c = Context.new(:username => 'admin', :password => 'sk8free', :protocol => 'http')
 c.login
-puts c.get('authentication/users')
+r = c.get('authentication/users')
+al = AtomResponseLoader::load_text(r)
+puts r
+puts al
 
 
 
