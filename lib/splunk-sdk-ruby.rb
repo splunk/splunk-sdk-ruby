790b3603d242a68f6b22960f870d2c19125076de
Ben Woodall <mail@benwoodall.com>
2012-07-19 12:37:18 -0700

    Added this file to the Splunk module.  This does away with the need to require other files inside lib/splunk-ruby-sdk/client.rb file.

--- lib/splunk-sdk-ruby.rb
+++ lib/splunk-sdk-ruby.rb
@@ -1,6 +1,8 @@
 require 'rubygems'
 require 'bundler/setup'
 
-require_relative 'splunk-sdk-ruby/aloader'
-require_relative 'splunk-sdk-ruby/context'
-require_relative 'splunk-sdk-ruby/client'
+module Splunk
+  require_relative 'splunk-sdk-ruby/aloader'
+  require_relative 'splunk-sdk-ruby/context'
+  require_relative 'splunk-sdk-ruby/client'
+end
