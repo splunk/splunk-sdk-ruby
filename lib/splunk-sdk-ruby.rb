ab3ad8c3cc0e7c39b77b2facab12b4e90126b44a
Frederick Ross <fross@splunk.com>
2012-12-20 09:47:44 -0800

    Wrote new AtomFeed to replace aloader.
    a loader took options to match particular hunks within the feed. XML paths are
    harder to deal with than Ruby objects, so I created AtomFeed, which produces
    a native Ruby object with fields metadata (a hash) and entries (a list of hashes).

--- lib/splunk-sdk-ruby.rb
+++ lib/splunk-sdk-ruby.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 
 module Splunk
   require_relative 'splunk-sdk-ruby/aloader'
+  require_relative 'splunk-sdk-ruby/atomfeed'
   require_relative 'splunk-sdk-ruby/context'
   require_relative 'splunk-sdk-ruby/client'
 end
---
+++ lib/splunk-sdk-ruby/atomfeed.rb
@@ -0,0 +1,144 @@
+  require 'nokogiri'
+
+  module Splunk
+
+  class AtomFeed
+    public
+
+    attr_reader :metadata
+    attr_reader :entries
+
+    def initialize(text)
+      # Sanity checks
+      raise ArgumentError, 'text is nil' if text.nil?
+      text = text.strip
+      raise ArgumentError, 'text size is 0' if text.size == 0
+
+      doc = Nokogiri::XML(text)
+      # Skip down to the content of the Atom feed. However, some of
+      # Splunk's endpoints don't include the <feed> element at the
+      # top level (notably search/jobs/<sid> in Splunk 4.3.x), so we
+      # have to check if <feed> is there or not before skipping.
+      if doc.root.name == "feed"
+        @metadata, @entries = read_feed(doc.root)
+      elsif doc.root.name == "entry"
+        @metadata = {}
+        @entries = [read_entry(doc.root)]
+      else
+        raise ArgumentError, 'root element of Atom must be feed or entry'
+      end
+    end
+
+    def read_feed(feed)
+      metadata = {"links" => {}, "messages" => []}
+      entries = []
+
+      feed.elements.each do |element|
+        if element.name == "entry"
+          entries << read_entry(element)
+        elsif element.name == "author"
+          # The author tag has the form <author><name>...</name></author>
+          # so we have to fetch the value out of the inside of it.
+          metadata["author"] = read_author(element)
+        elsif element.name == "generator"
+          # To handle elements of the form:
+          #     <generator build="144175" version="5.0.2"/>
+          metadata["generator"] = {}
+          element.attributes.each do |name, attribute|
+            metadata["generator"][name] = attribute.text
+          end
+        elsif element.name == "link"
+          rel, uri = read_link(element)
+          metadata["links"][rel] = uri
+        elsif element.name == "id"
+          metadata[element.name] = URI(element.children.to_s)
+        elsif element.name == "messages"
+          # No idea what these look like. Try to get one with messages.
+        else
+          metadata[element.name] = element.children.to_s
+        end
+      end
+
+      return metadata, entries
+    end
+
+    def read_entry(entry)
+      result = {"links" => {}}
+      entry.elements.each do |element|
+        name = element.name
+        if name == "link"
+          rel, uri = read_link(element)
+          result["links"][rel] = uri
+        else
+          value = read_entry_field(element)
+          result[name] = value
+        end
+      end
+
+      return result
+    end
+
+    def read_link(link)
+      # To handle elements of the form:
+      #     <link href="/%252FUsers%252Ffross%252Ffile%20with%20spaces"
+      # Note that the link is already URL encoded.
+      uri = URI(link.attributes['href'].text)
+      rel = link.attributes['rel'].text
+      return rel, uri
+    end
+
+    def read_entry_field(field)
+      if field.elements.length == 0 # This is a simple text field
+        return read_simple_entry_field(field)
+      elsif field.elements.length > 1
+        raise ArgumentError, "Entry fields should contain one element " +
+            "(found #{field.elements.length} in #{field.to_s}."
+      elsif field.name == "author"
+        return read_author(field)
+      end
+
+      value_element = field.elements[0]
+      if value_element.name == "dict"
+        return read_dict(value_element)
+      elsif value_element.name == "list"
+        return read_list(value_element)
+      end
+    end
+
+    def read_simple_entry_field(field)
+      if field.name == "id"
+        return URI(field.children.to_s)
+      else
+        return field.children.to_s
+      end
+    end
+
+    def read_dict(dict)
+      result = {}
+      dict.elements.each do |element|
+        key = element.attributes["name"].text
+        value = read_entry_field(element)
+        result[key] = value
+      end
+
+      return result
+    end
+
+    def read_list(list)
+      result = []
+      list.elements.each do |element|
+        value = read_entry_field(element)
+        result << value
+      end
+
+      return result
+    end
+
+    def read_author(author)
+      # The author tag has the form <author><name>...</name></author>
+      # so we have to fetch the value out of the inside of it.
+      return author.elements[0].text
+    end
+  end
+
+  end
