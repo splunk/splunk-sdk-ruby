439cf400563ce3ed3d52e0d50445efd931ecde17
Frederick Ross <fross@splunk.com>
2013-02-04 09:56:19 -0800

    Converted ResultsReader test data to be in JSON instead of Ruby.
    This will let me reuse the test data in other SDKs easily.

--- test/test_resultsreader.rb
+++ test/test_resultsreader.rb
@@ -1,5 +1,6 @@
 require_relative 'test_helper'
 require 'splunk-sdk-ruby'
+require 'json'
 
 include Splunk
 
@@ -11,35 +12,35 @@ class TestResultsReader < Test::Unit::TestCase
     puts "Nokogiri not installed. Skipping."
   end
 
-  test_data = eval(open("test/resultsreader_test_data.rb").read())
+  test_data = JSON::parse(open("test/resultsreader_test_data.json").read())
 
   xml_libraries.each do |xml_library|
-    test_data.each_entry do |test, expected|
-      version = test[0]
-      name = test[1]
-      test_name = "test_#{xml_library}_#{version.gsub(/\./, "_")}_#{name}"
-      define_method(test_name.intern()) do
-        Splunk::require_xml_library(xml_library)
-        file = File.open("test/data/results/#{version}/#{name}.xml")
-        reader = ResultsReader.new(file)
-        assert_equal(expected[:is_preview], reader.is_preview?)
-        assert_equal(expected[:fields], reader.fields)
+    test_data.each_entry do |version, tests|
+      tests.each_entry do |name, expected|
+        test_name = "test_#{xml_library}_#{version.gsub(/\./, "_")}_#{name}"
+        define_method(test_name.intern()) do
+          Splunk::require_xml_library(xml_library)
+          file = File.open("test/data/results/#{version}/#{name}.xml")
+          reader = ResultsReader.new(file)
+          assert_equal(expected["is_preview"], reader.is_preview?)
+          assert_equal(expected["fields"], reader.fields)
 
-        n_results = 0
-        reader.each_with_index do |result, index|
-          n_results += 1
-          # The assert of the full data structure below works, but
-          # by default Test::Unit doesn't print the diff of large
-          # data structures, so for debugging purposes it's much
-          # nicer to have each key checked individually as well.
-          expected[:results][index].each_entry do |key, value|
-            assert_equal([index, key, value],
-                         [index, key, result[key]])
+          n_results = 0
+          reader.each_with_index do |result, index|
+            n_results += 1
+            # The assert of the full data structure below works, but
+            # by default Test::Unit doesn't print the diff of large
+            # data structures, so for debugging purposes it's much
+            # nicer to have each key checked individually as well.
+            expected["results"][index].each_entry do |key, value|
+              assert_equal([index, key, value],
+                           [index, key, result[key]])
+            end
+            assert_equal(expected["results"][index], result)
           end
-          assert_equal(expected[:results][index], result)
-        end
 
-        assert_equal(expected[:results].length, n_results)
+          assert_equal(expected["results"].length, n_results)
+        end
       end
     end
   end
